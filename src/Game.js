06ef57070ce7102a221af6d9f73d9f6ae01580d6
Sandy <sangzicao@gmail.com>
2019-03-22 10:32:26 -0700

    add comments

--- src/Game.js
+++ src/Game.js
@@ -10,9 +10,9 @@ class Game extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dice: Array.from({ length: NUM_DICE }),
-      locked: Array(NUM_DICE).fill(false),
-      rollsLeft: NUM_ROLLS,
+      dice: Array.from({ length: NUM_DICE }), // [undefined, undefined, undefined, undefined, undefined]
+      locked: Array(NUM_DICE).fill(false), // [false, false, false, false, false ]
+      rollsLeft: NUM_ROLLS, //3
       scores: {
         ones: undefined,
         twos: undefined,
@@ -37,9 +37,12 @@ class Game extends Component {
   roll(evt) {
     // roll dice whose indexes are in reroll
     this.setState(st => ({
+      // Based on locked status, if T, don't roll, if F reroll
       dice: st.dice.map(
         (d, i) => st.locked[i] ? d : Math.ceil(Math.random() * 6)),
+      // if rollsLeft value reaches 0, then all the dice are locked
       locked: st.rollsLeft > 1 ? st.locked : Array(NUM_DICE).fill(true),
+      // decrease rollsLeft by 1 each turn
       rollsLeft: st.rollsLeft - 1,
     }));
   }
@@ -47,6 +50,8 @@ class Game extends Component {
   toggleLocked(idx) {
     // toggle whether idx is in locked or not
     this.setState(st => ({
+      // IS THIS A WEIRD FLEX??
+      // Why can't you just slice all and toggle at idx
       locked: [
         ...st.locked.slice(0, idx),
         !st.locked[idx],
@@ -58,8 +63,11 @@ class Game extends Component {
   doScore(rulename, ruleFn) {
     // evaluate this ruleFn with the dice and score this rulename
     this.setState(st => ({
+      // Calculate score for given rulename with ruleFn, save that into scores
       scores: { ...st.scores, [rulename]: ruleFn(this.state.dice) },
+      // Reset the rollsLeft back to start
       rollsLeft: NUM_ROLLS,
+      // Reset the locked status for the dice back to unlocked for all
       locked: Array(NUM_DICE).fill(false),
     }));
     this.roll();
